abb845d904a640683c3113f5b141b85588b0dfb3
Khaled_Hoshan <khaledjmk617@gmail.com>
2024-05-21 00:12:35 +0300

    add whatgpt3 and feature components and styles

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
-import './feature.css'
+import React from "react";
+import "./feature.css";
 
-function Fearture() {
+function Fearture({ title, text }) {
   return (
-    <div>
-      Feature
+    <div className="gpt3__features-container__feature">
+      <div className="gpt3__features-container__feature-title">
+        <h1>{title}</h1>
+      </div>
+      <div className="gpt3__features-container_feature-text">
+        <p>{text}</p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Fearture
+export default Fearture;
